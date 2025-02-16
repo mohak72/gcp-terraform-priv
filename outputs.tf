3817b27e0a612dc4857191723269ff046b4102e3
mohak72 <39758636+mohak72@users.noreply.github.com>
2025-02-16 10:45:57 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,55 @@
+######################################
+# 🔹 General Outputs
+######################################
+
+output "project_id" {
+  description = "The GCP Project ID where resources were deployed"
+  value       = var.project_id
+}
+
+output "region" {
+  description = "The GCP region where resources were deployed"
+  value       = var.region
+}
+
+output "zone" {
+  description = "The GCP zone where resources were deployed"
+  value       = var.zone
+}
+
+######################################
+# 🔹 Block Storage Outputs
+######################################
+
 output "gke_disk_name" {
-  description = "The created Persistent Disk for GKE"
+  description = "The name of the created Persistent Disk"
   value       = module.block_storage.disk_name
 }
 
 output "gke_pvc_name" {
-  description = "The created Persistent Volume Claim (PVC)"
+  description = "The name of the created Persistent Volume Claim (PVC)"
   value       = module.block_storage.pvc_name
 }
 
+######################################
+# 🔹 Istio Outputs
+######################################
+
 output "istio_static_ip" {
-  description = "The reserved static IP for Istio Ingress"
+  description = "The reserved static IP for Istio Ingress Gateway"
   value       = module.istio.istio_static_ip
 }
 
+######################################
+# 🔹 Pub/Sub Outputs
+######################################
+
 output "pubsub_topic_id" {
-  description = "The created Pub/Sub topic ID"
+  description = "The ID of the created Pub/Sub topic"
   value       = module.pubsub.topic_name
+}
+
+output "pubsub_subscription_id" {
+  description = "The ID of the created Pub/Sub subscription"
+  value       = module.pubsub.subscription_name
 }
