d05037ca3d9e6568955c82ed81fea4ff4fac44a3
mohak72 <39758636+mohak72@users.noreply.github.com>
2025-02-16 10:38:18 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,19 +1,59 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-variable "zone" { type = string }
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
 
-# Block Storage Variables
-variable "disk_name" { type = string }
-variable "disk_size_gb" { type = number }
-variable "disk_type" { type = string }
-variable "pvc_name" { type = string }
-variable "pvc_storage_class" { type = string }
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
 
-# Istio Variables
-variable "static_ip_name" { type = string }
-variable "istio_namespace" { type = string }
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+}
 
-# Pub/Sub Variables
-variable "topic_name" { type = string }
-variable "subscription_name" { type = string }
-variable "ack_deadline_seconds" { type = number }
+# Istio Module Variables
+variable "static_ip_name" {
+  description = "Static IP Name for Istio"
+  default = ""
+  type        = string
+}
+
+variable "istio_namespace" {
+  description = "Namespace for Istio"
+  default = ""
+  type        = string
+}
+
+# Pub/Sub Module Variables
+variable "topic_name" {
+  description = "Name of the Pub/Sub topic"
+  default = ""
+  type        = string
+}
+
+variable "subscription_name" {
+  description = "Name of the Pub/Sub subscription"
+  default = ""
+  type        = string
+}
+
+# Block Storage Module Variables
+variable "disk_name" {
+  description = "GKE Persistent Disk Name"
+  default = ""
+  type        = string
+}
+
+variable "disk_size_gb" {
+  description = "Disk Size in GB"
+  default = ""
+  type        = number
+}
+
+variable "disk_type" {
+  description = "Type of Persistent Disk (pd-ssd, pd-standard)"
+  default = ""
+  type        = string
+}
